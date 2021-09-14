373ec7a58364e220b690a0d2ae4a93cf3e643968
Alexey Rusakov <rusal@bk.ru>
2021-09-14 17:16:36 +0300

    add praktika

---
+++ Praktika1/01/Praktika1_01_01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Praktika1_01_01
+{
+  class Program
+  {
+    struct Person
+    {
+      public string firstName;
+      public string lastName;
+      public int age;
+      static string secret;
+
+      public Person(string firstName, string lastName, int age)
+      {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        secret = "ВАЗЕЛИН";
+      }
+
+      public override string ToString()
+      {
+        return $"Фамилия: {this.lastName}  Имя: {this.firstName} ест {secret}" ;
+      }
+
+
+       public void setSecret(string str1)
+      {
+        secret = str1;
+      }
+    }
+
+    static void Main(string[] args)
+    {
+
+      Person p1 = new Person("Mersov", "Andrey", 66); 
+      Person p2 = new Person("Русаков", "Алексей", 36); 
+      p2.setSecret("Ёжика");
+      Console.WriteLine(p1);
+      Console.WriteLine(p2);
+    }
+  }
+}
